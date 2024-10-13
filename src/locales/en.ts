d0dd361c076706ca7ad60d6d69a66172ceffc58b
nevengh <56957050+nevengh@users.noreply.github.com>
2024-10-13 06:03:07 -1100

    add locales

---
+++ src/locales/en.ts
@@ -0,0 +1,38 @@
+import { Translations } from "../types";
+
+const en: Translations = {
+  Home: "Home",
+  About: "About Us",
+  ContactUs: "Contact Us",
+  Services: "Services",
+  ourClients: "Our Clients",
+  English: "English",
+  Arabic: "Arabic",
+  name:"Name",
+  email:"Email",
+  phone:"Phone Number",
+  whatsApp:"WhatsApp Number",
+  Message:"Message",
+  submit:"Submit",
+  about_text_1:"With more than 5 years of experience, Golden Click is considered the best digital marketing agency in uae, especially in medical marketing with the highest quality in various digital marketing campaigns or social media marketing",
+  about_text_2:"Golden Click is a leading marketing agency in medical marketing with the latest technology, equipment, and a highly experienced and efficient digital marketing team to provide all social media marketing services such as :",
+  VideoProduction:"Video Production",
+  video_production_desc:"we provide all media production services with the highest quality with a professional montage kit",
+  mediabuying:"Media Buying",
+  mediabuying_desc:"we have a team of experts in all types of paid ads",
+  ContentCreation:"Content Creation",
+  ContentCreation_desc:"we brand for your business through all types of content creation like social media content creation or seo content writing",
+  webdevelopment:"web development",
+  webdevelopment_desc:"Over the years, we have been one of the best web development companies in the uae , designing and optimizing websites to rank high on Google and drive results .",
+  OurServices:"Our Services",
+  PaidAds:"Paid Ads",
+  MediaHead:"Our Works",
+  GoldenClick:"Golden Click",
+  NoVideoAvailable:"No Video Available",
+  CallUs:"Call Us",
+  Address:"Official Address",
+  EmailUs:"Email Us",
+  
+};
+
+export default en;

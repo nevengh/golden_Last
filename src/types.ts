60d280b02c9cb34e2199c89b35c3ef8826840e31
nevengh <56957050+nevengh@users.noreply.github.com>
2024-10-13 06:19:48 -1100

    add types.ts

---
+++ src/types.ts
@@ -0,0 +1,38 @@
+// import { ReactNode } from "react";
+
+// types.ts
+export interface Translations{
+
+    Home: string;
+    About:string;
+    ContactUs:string;
+    Services:string;
+    ourClients:string;
+    English:string;
+    Arabic:string;
+    name:string;
+    phone:string;
+    email:string;
+    whatsApp:string;
+    Message:string;
+    submit:string;
+    about_text_1:string;
+    about_text_2:string;
+    VideoProduction:string;
+    video_production_desc:string;
+    mediabuying:string;
+    mediabuying_desc:string;
+    ContentCreation:string;
+    ContentCreation_desc:string;
+    webdevelopment:string;
+    webdevelopment_desc:string;
+    OurServices:string;
+    PaidAds:string;
+    MediaHead:string;
+    GoldenClick:string;
+    NoVideoAvailable:string;
+    CallUs:string;
+    Address:string;
+    EmailUs:string;
+    
+}

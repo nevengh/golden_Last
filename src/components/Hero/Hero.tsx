3c30f6a880ac0fda9ba95c70c970c08193cefae0
nevengh <56957050+nevengh@users.noreply.github.com>
2024-10-13 05:57:57 -1100

    add hero

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,14 @@
+import './Hero.css'
+import Form from '../Form/Form'
+
+const Hero = () => {
+  return (
+    <div>
+        <div className="hero_form">
+          <Form />
+        </div>
+    </div>
+  )
+}
+
+export default Hero
